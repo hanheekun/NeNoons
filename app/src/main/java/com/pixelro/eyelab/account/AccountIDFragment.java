47b48286e05be142752d1911deed0663aae36d23
svl <svl@smartvilllab.com>
2020-03-16 19:24:46 +0900

    add join

---
+++ app/src/main/java/com/pixelro/eyelab/account/AccountIDFragment.java
@@ -0,0 +1,42 @@
+package com.pixelro.eyelab.account;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.pixelro.eyelab.R;
+
+public class AccountIDFragment extends Fragment implements View.OnClickListener {
+
+    private final static String TAG = AccountIDFragment.class.getSimpleName();
+    private View mView;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_id, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mView = view;
+
+        view.findViewById(R.id.button_arrow_back_background).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_arrow_back_background:
+                getActivity().onBackPressed();
+                break;
+        }
+    }
+}
