dc1faae69a5299d1af17063d3a2b0553229be894
svl@smartvilllab.com <1qw21qW@1q>
2020-07-08 00:25:31 +0900

    add exercise new button

--- app/src/main/java/com/pixelro/eyelab/menu/exercise/ExerciseFragment.java
+++ app/src/main/java/com/pixelro/eyelab/menu/exercise/ExerciseFragment.java
@@ -2,11 +2,16 @@ package com.pixelro.eyelab.menu.exercise;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -19,7 +24,10 @@ import com.pixelro.eyelab.menu.exercise.ex02.Ex02Activity;
 import com.pixelro.eyelab.menu.exercise.ex03.Ex03Activity;
 import com.pixelro.eyelab.menu.exercise.ex04.Ex04Activity;
 import com.pixelro.eyelab.menu.exercise.history.ExHistoryActivity;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -30,6 +38,10 @@ public class ExerciseFragment extends Fragment implements View.OnClickListener {
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
     private View mView;
+
+    private TextView TvDate;
+    private TextView Tvnumber;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -73,6 +85,16 @@ public class ExerciseFragment extends Fragment implements View.OnClickListener {
 //            editor.commit();
 //        }
 
+        TvDate = (TextView)root.findViewById(R.id.textView_ex_date);
+
+        Date date_now = new Date(System.currentTimeMillis()); // 현재시간을 가져와 Date형으로 저장한다
+        // 년월일시분초 14자리 포멧
+        SimpleDateFormat fourteen_format = new SimpleDateFormat("yyyy.MM.dd");
+        TvDate.setText(fourteen_format.format(date_now));
+
+        Tvnumber = (TextView)root.findViewById(R.id.textView_ex_number);
+
+
         return root;
     }
 
@@ -81,32 +103,49 @@ public class ExerciseFragment extends Fragment implements View.OnClickListener {
         super.onResume();
 
         if (sharedPreferences.getBoolean(EYELAB.APPDATA.EXERCISE.EX_1_COMPLETE,false)){
-            ((Button)mView.findViewById(R.id.button_ex_01)).setBackgroundResource(R.drawable.ex_btn_02);
+            ((Button)mView.findViewById(R.id.button_ex_01)).setBackgroundResource(R.drawable.button_ex_01_c);
         }
         else{
-            ((Button)mView.findViewById(R.id.button_ex_01)).setBackgroundResource(R.drawable.ex_btn_01);
+            ((Button)mView.findViewById(R.id.button_ex_01)).setBackgroundResource(R.drawable.button_ex_01);
         }
 
         if (sharedPreferences.getBoolean(EYELAB.APPDATA.EXERCISE.EX_2_COMPLETE,false)){
-            ((Button)mView.findViewById(R.id.button_ex_02)).setBackgroundResource(R.drawable.ex_btn_02);
+            ((Button)mView.findViewById(R.id.button_ex_02)).setBackgroundResource(R.drawable.button_ex_02_c);
         }
         else{
-            ((Button)mView.findViewById(R.id.button_ex_02)).setBackgroundResource(R.drawable.ex_btn_01);
+            ((Button)mView.findViewById(R.id.button_ex_02)).setBackgroundResource(R.drawable.button_ex_02);
         }
 
         if (sharedPreferences.getBoolean(EYELAB.APPDATA.EXERCISE.EX_3_COMPLETE,false)){
-            ((Button)mView.findViewById(R.id.button_ex_03)).setBackgroundResource(R.drawable.ex_btn_02);
+            ((Button)mView.findViewById(R.id.button_ex_03)).setBackgroundResource(R.drawable.button_ex_03_c);
         }
         else{
-            ((Button)mView.findViewById(R.id.button_ex_03)).setBackgroundResource(R.drawable.ex_btn_01);
+            ((Button)mView.findViewById(R.id.button_ex_03)).setBackgroundResource(R.drawable.button_ex_03);
         }
 
         if (sharedPreferences.getBoolean(EYELAB.APPDATA.EXERCISE.EX_4_COMPLETE,false)){
-            ((Button)mView.findViewById(R.id.button_ex_04)).setBackgroundResource(R.drawable.ex_btn_02);
+            ((Button)mView.findViewById(R.id.button_ex_04)).setBackgroundResource(R.drawable.button_ex_04_c);
         }
         else{
-            ((Button)mView.findViewById(R.id.button_ex_04)).setBackgroundResource(R.drawable.ex_btn_01);
+            ((Button)mView.findViewById(R.id.button_ex_04)).setBackgroundResource(R.drawable.button_ex_04);
         }
+
+        //
+        int curTotalEXNumber = sharedPreferences.getInt(EYELAB.APPDATA.EXERCISE.EX_DAY_NUMBER,0);
+        Tvnumber.setText("");
+        String s= "오늘 ";
+        SpannableString ss1=  new SpannableString(s);
+        ss1.setSpan(new RelativeSizeSpan(1f), 0,s.length(), 0); // set size
+        Tvnumber.append(ss1);
+        s= "" + curTotalEXNumber;
+        ss1=  new SpannableString(s);
+        ss1.setSpan(new RelativeSizeSpan(2f), 0,s.length(), 0); // set size
+        ss1.setSpan(new ForegroundColorSpan(Color.BLACK), 0, s.length(), 0);// set color
+        Tvnumber.append(ss1);
+        s= " 개의 눈 운동을 하셨네요.";
+        ss1=  new SpannableString(s);
+        ss1.setSpan(new RelativeSizeSpan(1f), 0,s.length(), 0); // set size
+        Tvnumber.append(ss1);
 
     }
 
